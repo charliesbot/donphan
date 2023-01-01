4747c446485bec462b94967304c82f44419eb7bd
Charlie L <charliesbot@gmail.com>
2023-01-01 14:05:25 -0600

    migrate to expo

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
