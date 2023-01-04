deeb15dc1a7310dcad7a4892369006b4909d897c
Charlie L <charliesbot@gmail.com>
2023-01-03 22:42:45 -0600

    follow convention to setup lambdas in Vercel

---
+++ server/api/config.ts
@@ -0,0 +1,24 @@
+class MastodonConfigBuilder {
+  static build = (mastodonServerUrl: string) => {
+    return {
+      baseConfig: {
+        redirect_uri: "urn:ietf:wg:oauth:2.0:oob",
+        scope: "read write follow push",
+      },
+      oauthConfig: {
+        client: {
+          id: process.env.CLIENT_ID as string,
+          secret: process.env.CLIENT_SECRET as string,
+        },
+        auth: {
+          authorizeHost: `${mastodonServerUrl}/oauth/authorize`,
+          authorizePath: `${mastodonServerUrl}/oauth/authorize`,
+          tokenHost: `${mastodonServerUrl}/oauth/token`,
+          tokenPath: `${mastodonServerUrl}/oauth/token`,
+        },
+      },
+    };
+  };
+}
+
+export { MastodonConfigBuilder };
--- server/api/oauth.ts
+++ server/api/oauth.ts
@@ -3,14 +3,16 @@ import { AuthorizationCode } from "simple-oauth2";
 import { MastodonConfigBuilder } from "./config";
 
 export default async (request: VercelRequest, response: VercelResponse) => {
-  const mastodonServerUrl = request.query.mastodonServerUrl as string;
+  const mastodonServerUrl = request.query.mastodon_server_url as string;
   const config = MastodonConfigBuilder.build(mastodonServerUrl);
-  const client = new AuthorizationCode(config.clientConfig);
+  const client = new AuthorizationCode(config.oauthConfig);
 
   const authorizationUri = client.authorizeURL({
     ...config.baseConfig,
     state: "pseudo-random",
   });
+
+  console.log(authorizationUri);
 
   response.status(301).redirect(authorizationUri);
 };
