28630ae76af8406d8f7dfe89dc1f82dfdd757ebb
Charlie L <charliesbot@gmail.com>
2023-01-08 17:34:58 -0800

    android setup

--- app/App.tsx
+++ app/App.tsx
@@ -1,10 +1,23 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Button, StyleSheet, TextInput, View } from "react-native";
+import * as WebBrowser from "expo-web-browser";
+
+const onOpenOAuth = async () => {
+  await WebBrowser.openBrowserAsync(
+    "https://donphan-oauth.vercel.app/api/oauth"
+  );
+};
 
 export default function App() {
+  const [serverUrl, setServerUrl] = useState("");
   return (
     <View style={styles.container}>
-      <Text>Hola mundo</Text>
+      <TextInput
+        placeholder="Mastodon Server"
+        onChangeText={(text) => setServerUrl(text)}
+      />
+      <Button title="Login" onPress={() => alert(serverUrl)} />
       <StatusBar style="auto" />
     </View>
   );
